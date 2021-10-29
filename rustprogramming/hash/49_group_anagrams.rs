5e024a0cffadf17e3e1c5ebe1fb0c14e92e01d57
sre-priscilla <943459049@qq.com>
2021-10-29 22:45:39 +0800

    49

---
+++ rustprogramming/hash/49_group_anagrams.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<String, Vec<String>> = HashMap::with_capacity(strs.len());
+        for str in strs.iter() {
+            let mut counter: [u8; 26] = [0; 26];
+            for ch in str.chars() {
+                counter[ch as usize - 97] += 1;
+            }
+            let key: String = counter.iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join("_");
+            map.entry(key).or_insert(vec![]).push(str.clone());
+        }
+        return map.values().map(|v| (*v).clone()).collect();
+    }
+}
+
+fn main() {
+    let original_strs = vec!["eat", "tea", "tan", "ate", "nat", "bat"];
+    let strs: Vec<String> = original_strs.iter().map(|s| String::from(*s)).collect();
+    println!("{:?}", Solution::group_anagrams(strs));
+}
