10020fd847ee4c8dc7429db8f7569d485c0d2b9c
sre-priscilla <943459049@qq.com>
2021-11-03 23:43:04 +0800

    cpp 1

---
+++ codingpuzzles/1_two_sum.cc
@@ -0,0 +1,32 @@
+#include <map>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(nums[i]) != m.end()) {
+                res = {m[nums[i]], i};
+                return res;
+            }
+            m[target - nums[i]] = i;
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    // Solution solution();
+    vector<int> vec = { 2, 7, 11, 15 };
+    for_each(vec.begin(), vec.end(), [](int &i){ printf("%d\n", i); });
+    vector<int> res = Solution().twoSum(vec, 9);
+    for_each(res.begin(), res.end(), [](int &i){ printf("%d\n", i); });
+    return 0;
+}
