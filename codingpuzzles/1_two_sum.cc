613363b227f3a67f955974dcb4b4bea4fc4414b4
sre-priscilla <943459049@qq.com>
2021-11-06 22:26:05 +0800

    cpp 121

--- codingpuzzles/1_two_sum.cc
+++ codingpuzzles/1_two_sum.cc
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         vector<int> res;
         map<int, int> m;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) != m.end()) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.find(nums[i]) != m.end())
+            {
                 res = {m[nums[i]], i};
                 return res;
             }
@@ -22,11 +26,14 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     // Solution solution();
-    vector<int> vec = { 2, 7, 11, 15 };
-    for_each(vec.begin(), vec.end(), [](int &i){ printf("%d\n", i); });
+    vector<int> vec = {2, 7, 11, 15};
+    for_each(vec.begin(), vec.end(), [](int &i)
+             { printf("%d\n", i); });
     vector<int> res = Solution().twoSum(vec, 9);
-    for_each(res.begin(), res.end(), [](int &i){ printf("%d\n", i); });
+    for_each(res.begin(), res.end(), [](int &i)
+             { printf("%d\n", i); });
     return 0;
 }
