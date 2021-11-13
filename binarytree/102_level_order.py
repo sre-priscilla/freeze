f2d640adfcbb6797b1209f747221a9b85aeaf87f
sre-priscilla <943459049@qq.com>
2021-11-13 11:37:44 +0800

    python: leetcode[102]

---
+++ binarytree/102_level_order.py
@@ -0,0 +1,35 @@
+from typing import List
+from collections import deque
+
+class TreeNode:
+    
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        ret, queue = [], deque([root])
+        while len(queue) > 0:
+            n, nums = len(queue), []
+            for _ in range(n):
+                node = queue.popleft()
+                nums.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ret.append(nums)
+        return ret
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    print(Solution().levelOrder(root))
