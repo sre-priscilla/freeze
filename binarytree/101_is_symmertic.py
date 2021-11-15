3999559edea3c3e504fa90c34c9175007308f05a
sre-priscilla <943459049@qq.com>
2021-11-15 12:11:44 +0800

    python: leetcode[101]

--- binarytree/101_is_symmertic.py
+++ binarytree/101_is_symmertic.py
@@ -8,27 +8,20 @@ class TreeNode:
         self.right = right
 
 class Solution:
+
+    def dfs(self, a: TreeNode, b: TreeNode) -> bool:
+        if not (a and b):
+            return a is None and b is None
+        else:
+            return a.val == b.val and self.dfs(a.left, b.right) and self.dfs(a.right, b.left)
+        
+
     def isSymmetric(self, root: TreeNode) -> bool:
-        if root is None:
-            return True
-        queue: Deque[TreeNode] = deque([root.left, root.right])
-        while len(queue) > 0:
-            n = len(queue)
-            tmp = deque([])
-            for _ in range(n):
-                node: TreeNode = queue.popleft()
-                if node:
-                    queue.append(node.left, node.right)
-                tmp.append(node)
-            if len(tmp) % 2 != 0:
-                return False
-            while tmp:
-                if tmp[0] is None and tmp[-1] is None:
-                    tmp.pop()
-                    tmp.popleft()
-                elif tmp[0] and tmp[-1] and tmp[0].val == tmp[-1].val:
-                    tmp.pop()
-                    tmp.popleft()
-                else:
-                    return False
-        return True
+        return self.dfs(root.left, root.right)
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    print(Solution().isSymmetric(root))
