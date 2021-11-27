12fd2f3d391dd50130c4bd37c1164c3d2ce5dbf6
sre-priscilla <943459049@qq.com>
2021-11-27 15:14:46 +0800

    python: lc 547

---
+++ graph/union_find/quick_find.py
@@ -0,0 +1,34 @@
+class UnionFind:
+
+    def __init__(self, size: int):
+        self.root = [i for i in range(size)]
+
+    def find(self, x: int) -> int:
+        return self.root[x]
+
+    def union(self, x: int, y: int):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            for i, _ in enumerate(self.root):
+                if self.root[i] == root_y:
+                    self.root[i] = root_x
+        print(f'union({x} {y})', self.root)
+
+    def connected(self, x: int, y: int):
+        return self.find(x) == self.find(y)
+
+
+if __name__ == '__main__':
+    uf = UnionFind(10)
+    uf.union(1, 2)
+    uf.union(2, 5)
+    uf.union(5, 6)
+    uf.union(6, 7)
+    uf.union(3, 8)
+    uf.union(8, 9)
+    print(uf.connected(1, 5))
+    print(uf.connected(5, 7))
+    print(uf.connected(4, 9))
+    uf.union(9, 4)
+    print(uf.connected(4, 9))
